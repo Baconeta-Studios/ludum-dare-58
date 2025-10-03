dc4a5cc92fa2ad9b6a4927143bc3176931857733
Josh Villgren <36744690+baconeta@users.noreply.github.com>
2025-10-03 14:55:07 +1300

    Initial commit

---
+++ Assets/Scripts/Audio/CustomAudioSource.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace Audio
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class CustomAudioSource : MonoBehaviour
+    {
+        private AudioSource _self;
+
+        private void Awake()
+        {
+            DontDestroyOnLoad(this);
+        }
+
+        private void ResetData()
+        {
+            // Stop Audio
+            if (_self != null)
+            {
+                _self.Stop();
+                Destroy(gameObject);
+            }
+        }
+
+        public void Init(AudioMixerGroup group)
+        {
+            if (!_self) _self = GetComponent<AudioSource>();
+
+            _self.outputAudioMixerGroup = group;
+        }
+
+        public void PlayOnce(AudioClip clip, float volume)
+        {
+            if (!_self) _self = GetComponent<AudioSource>();
+            _self.PlayOneShot(clip,volume);
+            StartCoroutine(ResetObject(clip.length + 0.5f));
+        }
+
+        public void PlayLooping(AudioClip clip, float volume)
+        {
+            if (!_self) _self = GetComponent<AudioSource>();
+            _self.clip = clip;
+            _self.loop = true;
+            _self.volume = volume;
+            _self.Play();
+        }
+
+        public void StopAudio()
+        {
+            StartCoroutine(ResetObject(0f));
+        }
+
+        private IEnumerator ResetObject(float delay)
+        {
+            yield return new WaitForSeconds(delay);
+
+            // Now recycle the object
+            ResetData();
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/Singleton.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using UnityEngine;
+
+namespace Utils
+{
+    public class Singleton<T> : MonoBehaviour where T : Singleton<T>
+    {
+        /// <summary>
+        /// Singleton pattern
+        /// </summary>
+        // ReSharper disable once InconsistentNaming
+        protected static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindFirstObjectByType<T>();
+                    if (_instance == null)
+                    {
+                        GameObject newInstance = new();
+                        _instance = newInstance.AddComponent<T>();
+                        _instance.name = typeof(T).ToString();
+                        Debug.LogWarning(
+                            $"Static Instance was not found for {_instance.name} - A new object has been created.");
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        protected virtual void Awake()
+        {
+            if (!Application.isPlaying)
+            {
+                return;
+            }
+
+            if (_instance == null)
+            {
+                _instance = this as T;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        protected void OnDestroy()
+        {
+            if (_instance == this)
+            {
+                _instance = null;
+            }
+        }
+    }
+}
