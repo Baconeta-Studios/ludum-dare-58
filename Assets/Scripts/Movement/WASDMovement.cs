43fa55efa043846754ee2b86418dd412095cefd7
Steven Rutherford <steve.rutherford12@gmail.com>
2025-10-04 17:33:31 +1300

    Local Multiplayer works w
    Cameras are local to client, players are local to client.

--- Assets/Scripts/Movement/WASDMovement.cs
+++ Assets/Scripts/Movement/WASDMovement.cs
@@ -3,15 +3,21 @@ using UnityEngine;
 public class WASDMovement : MonoBehaviour
 {
     public float moveSpeed = 5f; // movement speed in units per second
+    public float moveX;
+    public float moveZ; 
+
+    public void Move(Vector2 move)
+    {
+        this.moveX = move.x;
+        this.moveZ = move.y;
+    }
 
     void Update()
     {
         // Get input axes (WASD mapped to Horizontal/Vertical by default)
-        float moveX = Input.GetAxis("Horizontal"); // A/D or Left/Right
-        float moveZ = Input.GetAxis("Vertical");   // W/S or Up/Down
 
         // Create movement vector
-        Vector3 move = new Vector3(moveX, 0, moveZ);
+        Vector3 move = new Vector3(this.moveX, 0, this.moveZ);
 
         // Move relative to world space
         transform.Translate(move * moveSpeed * Time.deltaTime, Space.World);
