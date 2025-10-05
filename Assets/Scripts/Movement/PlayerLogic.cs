ee322ea2fe5aa4e5d80dffc2a3c1bf228ac5eb5a
Josh Villgren <joshuavillgren@gmail.com>
2025-10-06 02:01:11 +1300

    added a logic script to handle the game state and scoring logic for the player

---
+++ Assets/Scripts/Movement/PlayerLogic.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Managers;
+using UnityEngine;
+
+namespace Movement
+{
+    public class PlayerLogic : MonoBehaviour
+    {
+        private List<string> _myObjectives;
+        private void Start()
+        {
+            Debug.Log("Start player spawning system");
+            GameManager.Instance.OnPlayerSpawned(name, gameObject);
+            
+            Invoke(nameof(GetObjectives), 1f);
+        }
+
+        private void GetObjectives()
+        {
+            _myObjectives = GameManager.Instance.GetMyObjectives(name);
+        }
+    }
+}
