3482d9005d6aa824f1a20557b902b5d2499ce412
Josh Villgren <joshuavillgren@gmail.com>
2025-10-06 22:27:11 +1300

    Update _a1fa7cddf5d5ef94b8bacf8838c9a6fa_4cad330df3b041179eff4cb8ab5129ff.cs

--- Assets/coherence/baked/Interop/_a1fa7cddf5d5ef94b8bacf8838c9a6fa_4cad330df3b041179eff4cb8ab5129ff.cs
+++ Assets/coherence/baked/Interop/_a1fa7cddf5d5ef94b8bacf8838c9a6fa_4cad330df3b041179eff4cb8ab5129ff.cs
@@ -30,7 +30,7 @@ namespace Coherence.Generated
         {
             if (dataSize != 16) {
                 throw new System.Exception($"Given data size is not equal to the struct size. ({dataSize} != 16) " +
-                    "for command with ID 66");
+                    "for command with ID 106");
             }
 
             var orig = new _a1fa7cddf5d5ef94b8bacf8838c9a6fa_4cad330df3b041179eff4cb8ab5129ff();
@@ -48,7 +48,7 @@ namespace Coherence.Generated
         public uint SenderParticipant { get; set; }
         public ClientID SenderClientID { get; set; }
         public long Frame { get; set; }
-        public uint GetComponentType() => 66;
+        public uint GetComponentType() => 106;
         public bool UsesMeta { get; set; }
 
         public IEntityMessage Clone()
